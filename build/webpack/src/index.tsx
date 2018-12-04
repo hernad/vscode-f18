7abeba2a8b17c43b23db0ae7fd0ef14a045de970
Ernad Husremovic <hernad@bring.out.ba>
2018-12-04 19:52:03 +0100

    pocinjem razumjevati TS, Promise

--- build/webpack/src/index.tsx
+++ build/webpack/src/index.tsx
@@ -28,12 +28,90 @@ for (const num of numbers) {
 }
 */
 
-ReactDOM.render(
-	<MySection>
-        <MyButton>Button 1</MyButton>
-        <MyButton>Button 2</MyButton>
-        <MyButton>Button 3</MyButton>
-        <MyButton>Button 4</MyButton>
-    </MySection>,
+// const mySection : React.ReactElement<any> = <MySection.Third />;
+
+
+/*
+
+var helloWorld = <div>Hello World!</div>;
+
+// And this is what the JSX syntax compiles into in JavaScript:
+var helloWorld = React.createElement(
+  "div",
+  null,
+  "Hello World!"
+);
+
+*/
+
+// And this is what the JSX syntax compiles into in JavaScript:
+/*
+const myThirdElement = React.createElement(
+	"MySection.Third",
+	null
+  );
+*/
+
+const propDugmeNaslov = "P2 Dugme from Var"; 
+const myThirdElement = <MySection.Third ptext={propDugmeNaslov} />
+
+interface  IMyContainerState {
+	items: string[];
+}
+type TResoveFn = (items: IMyContainerState) => void;
+
+const resolveFn = (resolve: TResoveFn) => {
+	setTimeout(() => {
+	   const posaljiItems: IMyContainerState = {items: ['First', 'Second', 'Third', 'pet']};
+	   resolve(posaljiItems);
+	}, 2000);
+}
+
+
+function fetchData() {
+	return new Promise( resolveFn );
+}
+
+
+
+class MyContainer extends React.Component<{}, IMyContainerState> {
+
+	public state: IMyContainerState = {
+		items: [ "nula" ]
+	};
+
+	// After the component has been rendered, make the
+	// call to fetch the component data, and change the
+	// state when the data arrives.
+	public componentDidMount() {
+		fetchData().then( value => {
+			const fetchedItems = value.items;
+			this.setState( {items: [...this.state.items, ...fetchedItems]} as IMyContainerState )
+		});
+	}
+
+	public render() {
+		return <MySection.MyList {...this.state} />;
+	}
+}
+
+/*
+const myCompInstance = ReactDOM.render(
+    myThirdElement,
 	document.getElementById('example')
 );
+
+setTimeout(() => {
+	(myCompInstance as React.Component).setState({
+		heading: "React OK",
+		content: "Done"
+	});
+}, 1000);
+*/
+
+const myCompInstance = ReactDOM.render(
+    <MyContainer/>,
+	document.getElementById('example')
+);
+
+// const domInstance = ReactDOM.findDOMNode(myCompInstance);
