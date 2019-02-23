01bd90367ee1e89db228bbdd92d0739d9dd708c4
Ernad Husremovic <hernad@bring.out.ba>
2019-02-23 12:21:29 +0100

    20190223.5

--- ext/constants.ts
+++ ext/constants.ts
@@ -5,21 +5,12 @@ export class Constants {
   public static PostgresGlobalStateKey = 'postgresql.connections';
 }
 
-export const revision = '20190223.4';
+export const revision = '20190223.5';
 
 export const execHashList: any = {
-  'linux-x64': 'b1daa1ae7575bdd86fedd41c11a7306a',
-  'linux-x86': 'e5d7cc9a02932c969ff9aaeb50bac0ad',
-  'windows-x64': '35adfc904d8e019aae0d6d423f0c1e2a',
-  'windows-x86': '13d6d89141466cc677cac6094709924e'
+  'linux-x64': '6fbf34aa7e2eb7f021bf7c7f61789ef2',
+  'linux-x86': '83d79ab4153750c50dddecf76ba23cd9',
+  'windows-x64': '242cabc88038523f67a2706b369c20f9',
+  'windows-x86': '69bc72b2f079a14f9c097418dad23308'
 };
 
-
-// F18-linux-x64_20190223.4.zip
-// b1daa1ae7575bdd86fedd41c11a7306a  tmp/F18
-// F18-linux-x86_20190223.4.zip
-// e5d7cc9a02932c969ff9aaeb50bac0ad  tmp/F18
-// F18-windows-x64_20190223.4.zip
-// 35adfc904d8e019aae0d6d423f0c1e2a  tmp/F18.exe
-// F18-windows-x86_20190223.4.zip
-// 13d6d89141466cc677cac6094709924e  tmp/F18.exe
