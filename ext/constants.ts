a5a44a8aba3f1255c25b2fad92c365ee8e48ecb3
Ernad Husremovic <hernad@bring.out.ba>
2019-03-20 16:49:50 +0100

    update ver .6

--- ext/constants.ts
+++ ext/constants.ts
@@ -5,28 +5,30 @@ export class Constants {
   public static PostgresGlobalStateKey = 'postgresql.connections';
 }
 
-const revisionS = '20190320.5';
+
+const revisionS = '20190320.6';
 const execHashListS: any = {
+  'linux-x64': '61e360c1ea91cef98ace5587c9b0892e',
+  'linux-x86': 'f287a3df59a68456ca2ce030bc3195c2',
+  'windows-x64': '6d688469e1a18298ade6fd0ee9ab5331',
+  'windows-x86': 'b57ee647810d83ae901c0739edabbe20'
+};
+
+
+const revisionX = '20190320.5';
+const execHashListX: any = {
   'linux-x64': 'ac6819fb1ee814fb5e7e4de56052c85d',
   'linux-x86': '71639f2774d92d76e0464cd1fa786d6b',
   'windows-x64': '7324273572ed9fb4c55afad0db5505c0',
   'windows-x86': '7072da58ee941674ad2fee75f7e7f2e7'
 };
 
+const revisionE = revisionX;
+const execHashListE = execHashListX;
 
-const revisionE = revisionS;
-const execHashListE = execHashListS;
+//const revisionX = revisionS;
+//const execHashListX = execHashListS;
 
-const revisionX = revisionS;
-const execHashListX = execHashListS;
-
-//const revisionX = '20190307.1';
-//const execHashListX: any = {
-//  'linux-x64': '870513e85917bd102a0a1ec9288b4329',
-//  'linux-x86': '2bd88996b9bece79eea240f174cec587',
-//  'windows-x64': '96f426d89cebd99b38feff08b247cc4a',
-//  'windows-x86': '5389398a31fb74b938e51b940fbca9db'
-//};
 
 
 const execHashListAll: any = {
