5bad01d0870724406dde4986cd0aa84a36feab76
Ernad Husremovic <hernad@bring.out.ba>
2020-09-24 09:42:57 +0200

    4.22.73 windows

--- ext/constants.ts
+++ ext/constants.ts
@@ -9,14 +9,19 @@ export class Constants {
 }
 
 
-// cf89a428c13d2790fa70a1d124e7b1ea  tmp/F18-klijent
-// F18-windows-x64_4.22.64.zip
-// c5ca0cccfa3fff0668187e885089d222  tmp/F18-klijent.exe
-// F18-windows-x86_4.22.64.zip
-// 5f2ca84c5b61df1072d73cfc3a02912a  tmp/F18-klijent.exe
-
-const revisionS = '4.22.72';
+const revisionS = '4.22.73';
 const execHashListS: any = {
+  'linux-x64': '0',
+  'linux-x64-prev' : 'f0ef32d054e694b2026256543dee3adb',
+  'linux-x64-prev-rev': '4.22.72',
+
+  'windows-x64': '17355cd38e71135c164390e9dd621e40',
+  'windows-x86': '026914b3656db7d5c9eafa1f42f05bbc',
+};
+
+
+const revisionE = '4.22.72';
+const execHashListE: any = {
   'linux-x64': 'f0ef32d054e694b2026256543dee3adb',
   'windows-x64': '0',
   'windows-x86': '0',
@@ -29,19 +34,11 @@ const execHashListS: any = {
 };
 
 
-const revisionE = '4.22.60';
-const execHashListE: any = {
+const revisionX = '4.22.60';
+const execHashListX: any = {
   'linux-x64': '56bf82507de44aa0261400060c32404b',
   'windows-x64': '0193afc4af06c5c922141d3b4a2e4b3b',
   'windows-x86': '8f57d6bd6b725efaeb96f7f59009c1a8'
-};
-
-
-const revisionX = '4.22.54';
-const execHashListX: any = {
-  'linux-x64': 'c78ba866980fd1b06783395444dd4959',
-  'windows-x64': 'f323249688a560aaec816372c16c600a',
-  'windows-x86': 'adb7667247a5c498567576af3b2fb42a'
 };
 
 
