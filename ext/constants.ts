3ba01c8e9ca02ce0b41ab936b448a9ef960abd74
Ernad Husremovic <hernad@bring.out.ba>
2024-11-28 10:47:29 +0100

    vscode-f18 6.5.1

--- ext/constants.ts
+++ ext/constants.ts
@@ -8,9 +8,9 @@ export class Constants {
   public static PostgresGlobalStateKey = 'postgresql.connections';
 }
 
-const revisionS = '4.24.53';
+const revisionS = '4.24.56';
 const execHashListS: any = {
-  'windows-x64-prev': 'e434c3a9e41f6a1b7a2bf3b23619ee12',
+  'windows-x64-prev': 'e6f14cfa02981898fe397282044138ce',
   'windows-x64-prev-rev': '4.24.29',
   'linux-x64': 'c4654073ef3f77004463a29182b58898',
   //'linux-x64-prev': 'c1592edae4bc1dc2f1c7bfa9f13825ca',
