b5ad8c6b3f952130509dbab7e60b9ae955568dab
Ernad Husremovic <hernad@bring.out.ba>
2025-01-22 14:51:13 +0100

    6.5.7

--- ext/constants.ts
+++ ext/constants.ts
@@ -8,13 +8,14 @@ export class Constants {
   public static PostgresGlobalStateKey = 'postgresql.connections';
 }
 
-const revisionS = '4.24.58';
+const revisionS = '4.24.59';
 const execHashListS: any = {
-  'windows-x64-prev': 'e6f14cfa02981898fe397282044138ce',
-  'windows-x64-prev-rev': '4.24.29',
-  'linux-x64': 'd28c4e39b49812a219ac259a05614d6a',
-  //'linux-x64-prev': 'c1592edae4bc1dc2f1c7bfa9f13825ca',
-  //'linux-x64-prev-rev': '4.24.5',
+  'windows-x64': 'd9f24b4a1b59100869793879f750c80a',
+  //'windows-x64-prev': 'e6f14cfa02981898fe397282044138ce',
+  //'windows-x64-prev-rev': '4.24.29',
+  //'linux-x64': 'd28c4e39b49812a219ac259a05614d6a',
+  'linux-x64-prev': 'd28c4e39b49812a219ac259a05614d6a',
+  'linux-x64-prev-rev': '4.24.58',
   'windows-x86': '0',
   'windows-x86-prev': '32f4e15b847753b73f84c587b59ebd31',
   'windows-x86-prev-rev': '4.23.41',
